e488c2a8629ba6302f7c6c91b622c8ff888f2f8b
Logan McCalley <mccalleylogan@gmail.com>
2022-08-26 10:50:16 -0400

    added get and post

--- functions/src/Tasks.js
+++ functions/src/Tasks.js
@@ -1,11 +1,31 @@
+import { tasks } from "firebase-functions/v1";
+import dbConncect from "./dbConnect"
 
-export function getTasks(req,res){
-    res.send('tasks')
+
+export async function getTasks(req,res){
+    const db = dbConncect()
+    const collection= await db.collection('tasks').get()
+    .catch(err => res.status(500).send(err));
+    const tasks = collection.docs.map(doc => {
+        let task = doc.data()
+        task.id = doc.id
+        return task
+    })
+    res.send(tasks)
 }
 
-export function createTask(req,res){
+export async function createTask(req,res){ //later we will add userId and timestamp
     const newTask = req.body
-    res.status(201).send('Task Added')
+    if(!newTask || !newTask.task){
+        res.status(400).send({success:false, message: 'invalid request'})
+        return;
+    }
+    const db = dbConncect()
+   await db.collection('tasks').add(newTask)
+    .catch(err => res.status(500).send(err))
+    res.status(201)
+    getTasks(req,res) //send back the full list of tasks
+    
 }
 
 export function updateTask(req,res){
